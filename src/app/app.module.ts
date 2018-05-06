6cb2abc3a9704c6d65897108c84f7deed1bc9432
EC2 Default User <ec2-user@ip-172-31-21-34.us-east-2.compute.internal>
2018-05-06 22:13:10 +0000

    Split event-list and event-thumbnail components up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html' 
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr>
+        <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+    </div>
+    `
 })
 export class EventsListComponent {
     
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
@@ -18,6 +24,11 @@ export class EventsListComponent {
             city: 'London',
             country: 'England'
         }
+    }
+    
+    
+    handleEventClicked(data) {
+        console.log('received:', data)
     }
     
 }
