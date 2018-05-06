92395a073ee994771ab08532b5e09f27aa38781e
EC2 Default User <ec2-user@ip-172-31-21-34.us-east-2.compute.internal>
2018-05-06 21:47:20 +0000

    Initial upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component'
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app-root',
+  template: '<events-list></events-list>'
+
+})
+export class EventsAppComponent {
+  title = 'app';
+}
