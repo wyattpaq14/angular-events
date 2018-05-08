096b16d58482490aace87cb59c4279033c23d570
EC2 Default User <ec2-user@ip-172-31-21-34.us-east-2.compute.internal>
2018-05-08 00:42:17 +0000

    Added toastr and eventService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 
+import { EventService } from './events/shared/event.service'
+
 @NgModule({
   declarations: [
     EventsAppComponent,
@@ -16,7 +18,7 @@ import { NavBarComponent } from './nav/navbar.component'
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
